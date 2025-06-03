094606d76747b70d873892c663d3e5c9eb4adabe
Huynh Kim <huynhnk01@gmail.com>
2025-06-03 10:47:54 +0700

    feature1 update vite config for ngrok and mobile

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $host = Request::getHost();
+
+        if (!str_ends_with($host, '.local')) {
+            URL::forceScheme('https'); // dùng khi cần force schema https để có thể redirect đúng url
+            // URL::forceRootUrl(config('app.url')); // dùng khi cần force action trong form đúng với APP_URL
+        }
     }
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,50 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import fs from 'fs';
+
+/**
+ * server.host nên để là 0.0.0.0 hoặc true để chấp nhận tất cả domain (*)
+ * server.cors
+ *     - true nếu vite vs web(app_url) chaỵ khác domain (*)
+ *     - false hoặc ko cần đề cập nếu vite vs web(app_url) chaỵ cùng domain
+ * server.hmr.host
+ *     - nếu là domain ảo thì điện thoại không xem được
+ *     - nếu là IP thì điện thoại xem được (*)
+ *         - domain ảo cũng xem được luôn
+ *         - nếu là http thì máy tính hoặc điện thoại truy cập trực tiếp vào http://IP
+ *         - nếu là https thì máy tính hoặc điện thoại truy cập gián tiếp vào https://IP:5173
+ * server.hmr.protocol
+ *     - nếu là http thì dùng ws
+ *     - nếu là https thì dùng wss
+ * server.https
+ *     - nếu không dùng https thì cần comment phần này đi
+ *     - nếu dùng https thì cần phải có
+ *         - đồng bộ https cho các link asset để load được css và js
+ * 
+ * *** Lưu ý: IP có thể thay đổi chỉ cần set server.host là 0.0.0.0 và khi chạy npm run dev sẽ hiện ra IP mới
+ */
 
 export default defineConfig({
+    server: {
+        // host: 'jobportal.local',
+        host: '0.0.0.0',
+        port: 5173,
+        cors: true,
+        hmr: {
+            // host: 'jobportal.local',
+            host: '192.168.1.3', // IP có thể thay đổi
+            // protocol: 'ws',
+            protocol: 'wss',
+        },
+        watch: {
+            usePolling: true,
+            interval: 1000,
+        },
+        https: {
+            key: fs.readFileSync('./jobportal.local-key.pem'),
+            cert: fs.readFileSync('./jobportal.local.pem'),
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
